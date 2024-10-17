c23aef98e6d914f7afa87d97397dd790f08a6bf4
DLujan04 <A01254903@tec.mx>
2024-10-16 17:06:41 -0700

    Transactions and authorization routes

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from dotenv import load_dotenv
-from routes.routes import routes_blueprint  
+from routes.routes import routes_blueprint
+from routes.authorization import authorization_blueprint  
+from routes.transactions import transactions_blueprint  
 import os
 
 # Cargar variables de entorno desde el archivo .env
@@ -12,8 +14,10 @@ app = Flask(__name__)
 # Configurar el puerto desde las variables de entorno o usar 5000 por defecto
 PORT = int(os.getenv("PORT", 5000))
 
-# Registrar el blueprint después de crear la instancia Flask
+# Registrar los blueprints después de crear la instancia Flask
 app.register_blueprint(routes_blueprint)
+app.register_blueprint(authorization_blueprint)  
+app.register_blueprint(transactions_blueprint)  
 
 @app.route('/')
 def home():
